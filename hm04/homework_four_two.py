7e433eaddb193804c899fc643835be431f6828ef
MykolaSavchyn <angprogkar@gmail.com>
2025-12-20 12:26:07 +0200

    Improved homework four two and added homework four three

--- hm04/homework_four_two.py
+++ hm04/homework_four_two.py
@@ -3,15 +3,15 @@
 # [] => 0
 # [0, 1, 7, 2, 4, 8] => 88
 
-my_list = [1, 3, 5]
+my_list = [0, 1, 7, 2, 4, 8]
 
 my_list_pair = []
 
 if len(my_list) == 0:
-    print(len(my_list))
+    print(my_list, len(my_list), sep=' => ')
 else:
     for ind, item in enumerate(my_list):
         if ind%2 == 0:
             my_list_pair.append(item)
     # print(my_list_pair)
-    print(sum(my_list_pair)*my_list[-1])
+    print(my_list, sum(my_list_pair)*my_list[-1], sep=' => ')
